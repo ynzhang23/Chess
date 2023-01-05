3d630dc1b9c5a5ea6a16c70fe8dc875fcf29f10b
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-01-05 18:19:55 +0800

    Initialize pieces onto the board and method to print board

--- lib/board.rb
+++ lib/board.rb
@@ -1,21 +1,81 @@
 # frozen-string-literal: true
 
-# Contains the win condition, constraints and board data
+require 'pry-byebug'
 
+require './lib/pieces/rook'
+require './lib/pieces/knight'
+require './lib/pieces/bishop'
+require './lib/pieces/queen'
+require './lib/pieces/king'
+require './lib/pieces/pawn'
+
+# Contains the win condition, constraints and board data
 class Board
+  attr_reader :positions
+  
   def initialize
     @positions = Array.new(8){ Array.new(8, '-')}
   end
 
+  def place_all_pieces
+    place_all_rooks
+    place_all_knights
+    place_all_bishops
+    place_all_queens
+    place_all_kings
+    place_all_pawns
+  end
+
+  def place_all_rooks
+    @positions[0][0] = WhiteRook.new(0, 0)
+    @positions[0][7] = WhiteRook.new(0, 7)
+    @positions[7][0] = BlackRook.new(7, 0)
+    @positions[7][7] = BlackRook.new(7, 7)
+  end
+
+  def place_all_knights
+    @positions[0][1] = WhiteKnight.new(0, 1)
+    @positions[0][6] = WhiteKnight.new(0, 6)
+    @positions[7][1] = BlackKnight.new(7, 1)
+    @positions[7][6] = BlackKnight.new(7, 6)
+  end
+
+  def place_all_bishops
+    @positions[0][2] = WhiteBishop.new(0, 2)
+    @positions[0][5] = WhiteBishop.new(0, 5)
+    @positions[7][2] = BlackBishop.new(7, 2)
+    @positions[7][5] = BlackBishop.new(7, 5)
+  end
+
+  def place_all_queens
+    @positions[0][3] = WhiteQueen.new(0, 3)
+    @positions[7][3] = BlackQueen.new(7, 3)
+  end
+
+  def place_all_kings
+    @positions[0][4] = WhiteKing.new(0, 4)
+    @positions[7][4] = BlackKing.new(7, 4)
+  end
+
+  def place_all_pawns
+    @positions[1].map!.with_index do |position, index|
+      position = WhitePawn.new(1, index)
+    end
+    @positions[6].map!.with_index do |position, index|
+      position = BlackPawn.new(6, index)
+    end
+  end
+
   def print_board
-    rank_8 = @positions[7].join(' ')
-    rank_7 = @positions[6].join(' ')
-    rank_6 = @positions[5].join(' ')
-    rank_5 = @positions[4].join(' ')
-    rank_4 = @positions[3].join(' ')
-    rank_3 = @positions[2].join(' ')
-    rank_2 = @positions[1].join(' ')
-    rank_1 = @positions[0].join(' ')
+    rank_8 = filter_symbol(@positions[7]).join(' ')
+    rank_7 = filter_symbol(@positions[6]).join(' ')
+    rank_6 = filter_symbol(@positions[5]).join(' ')
+    rank_5 = filter_symbol(@positions[4]).join(' ')
+    rank_4 = filter_symbol(@positions[3]).join(' ')
+    rank_3 = filter_symbol(@positions[2]).join(' ')
+    rank_2 = filter_symbol(@positions[1]).join(' ')
+    rank_1 = filter_symbol(@positions[0]).join(' ')
+
     puts "
     ⚉ A B C D E F G H ⚉
     8 #{rank_8} 8
@@ -29,4 +89,19 @@ class Board
     ⚉ A B C D E F G H ⚉
     "
   end
+
+  def filter_symbol(array)
+    array.map do |position|
+      unless position == '-'
+        position.symbol 
+      else
+      position
+      end
+    end
+  end
+
 end
+
+board = Board.new
+board.place_all_pieces
+board.print_board

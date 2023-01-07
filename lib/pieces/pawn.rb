ec751ee767daa636d82956ffecf4262dff037fc3
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-01-07 22:22:13 +0800

    Create methods for movements on board

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -2,14 +2,46 @@
 
 # Template and shared method for a Pawn
 class Pawn
-  attr_reader :symbol
+  attr_reader :symbol, :next_moves, :color
+  attr_accessor :current_position
 
   def initialize(start_position, symbol, color)
     @symbol = symbol
     @color = color
     @start_position = start_position
     @current_position = start_position
-    @next_move = nil
+    @next_moves = []
+  end
+
+  # Update piece location on board
+  def update_position(board, new_position, old_position)
+    @current_position = new_position
+    board.positions[new_position[0]][new_position[1]] = self
+    board.positions[old_position[0]][old_position[1]] = '-'
+    update_next_moves(board)
+    board.print_board
+  end
+
+  # Updates @next_moves with current location
+  def update_next_moves(board)
+    rank = @current_position[0]
+    file = @current_position[1]
+    # One ahead
+    @next_moves.push([rank + 1, file]) if valid_move?(rank + 1, file) && board.positions[rank + 1][file] == '-'
+    # Two ahead when yet to move
+    if rank == 1 && board.positions[rank + 1][file] == '-' && board.positions[rank + 2][file] == '-'
+      @next_moves.push([rank + 2, file])
+    end
+    # When taking diagonal left
+    @next_moves.push([rank + 1, file - 1]) if valid_move?(rank +  1, file - 1) && board.positions[rank - 1][file - 1] != '-'
+    # When taking diagonal right
+    @next_moves.push([rank + 1, file + 1]) if valid_move?(rank +  1, file + 1) && board.positions[rank + 1][file + 1] != '-'
+  end
+
+  # Test if space is on board
+  def valid_move?(rank, file)
+    return false if rank < 0 || rank > 7 || file < 0 || file > 7
+    true
   end
 end
 
